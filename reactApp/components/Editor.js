b052ad267a066474618b68f93bd709f791dc4fc4
Adam Ripley <aripley@wharton.upenn.edu>
2017-07-24 13:17:28 -0700

    editor comp

---
+++ reactApp/components/Editor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TextEdit from './TextEdit.js';
+import Toolbar from './Toolbar.js';
+
+class Editor extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => {}}>Back to Documents Portal</button>
+        <h1> This is the document name </h1>
+        <p> Sharable Document ID: 69696969 </p>
+        <button onClick={() => {}}>Save Changes</button>
+        <Toolbar />
+        <TextEdit />
+      </div>
+    );
+  }
+}
+
+export default Editor;
