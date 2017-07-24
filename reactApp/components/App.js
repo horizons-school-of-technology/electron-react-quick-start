46a6c069e4c43a8e2e33464e545a5384d788f3d6
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-24 13:18:18 -0700

    fixed merge conflicts

--- reactApp/components/App.js
+++ reactApp/components/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import EditorView from './EditorView.js';
-
-class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-
-      };
-    }
-
-    render() {
-      return (
-          <EditorView />
-      );
-    }
-}
-=======
-import EditorContainer from '../containers/EditorContainer.js';
 
 const App = () => {
   return (
-      <EditorContainer />
+      <EditorView />
   );
 };
->>>>>>> f7d720580d9b03d191276f69dbd7b07e3614c1d6
 
 export default App;
