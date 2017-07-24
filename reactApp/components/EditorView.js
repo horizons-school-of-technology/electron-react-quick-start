e140c19de8233cd4ce0e6fcb03cab9a29df53e23
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-24 13:20:15 -0700

    editor and view changes

--- reactApp/components/EditorView.js
+++ reactApp/components/EditorView.js
@@ -2,24 +2,7 @@ import React from 'react';
 import TextEdit from './TextEdit.js';
 import Toolbar from './Toolbar.js';
 
-<<<<<<< HEAD:reactApp/components/EditorView.js
 class EditorView extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-
-      };
-    }
-
-    render() {
-      return (
-        <div>
-          <TextEdit />
-        </div>
-      );
-    }
-=======
-class Editor extends React.Component {
   render() {
     return (
       <div>
@@ -32,7 +15,6 @@ class Editor extends React.Component {
       </div>
     );
   }
->>>>>>> f7d720580d9b03d191276f69dbd7b07e3614c1d6:reactApp/components/Editor.js
 }
 
 export default EditorView;
