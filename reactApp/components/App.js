9f3e9b66d02eb5df4b6626fe9ec45c0a4cfbf6c1
Adam Ripley <aripley@wharton.upenn.edu>
2017-07-24 11:10:55 -0700

    editor work

--- reactApp/components/App.js
+++ reactApp/components/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Editor from './Editor.js';
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-      };
-    }
+    };
+  }
 
-    render() {
-      return (
-          <Editor />
-      );
-    }
+  render() {
+    return (
+      <Editor />
+    );
+  }
 }
 
 export default Editor;
--- reactApp/components/Editor.js
+++ reactApp/components/Editor.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 class Editor extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-      };
-    }
+    };
+  }
 
-    render() {
-      return (
-        <div>
-          In Editor
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        {/* <input type="submit">Back to Documents Portal</input>
+        <h1> This is the document name </h1>
+        <h1> Sharable Document ID: 69696969 </h1>
+        <input type="submit">Save Changes</input> */}
+        <button onClick={() => {}}>Back to Documents Portal</button>
+        <h1> This is the document name </h1>
+        <p> Sharable Document ID: 69696969 </p>
+        <button onClick={() => {}}>Save Changes</button>
+      </div>
+    );
+  }
 }
 
 export default Editor;
