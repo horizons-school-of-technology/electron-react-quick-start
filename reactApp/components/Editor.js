555764dd754dd575f974b116a8ba2082cf745c3c
Adam Ripley <aripley@wharton.upenn.edu>
2017-07-24 11:26:30 -0700

    added toolbar component

--- reactApp/components/Editor.js
+++ reactApp/components/Editor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextEdit from './TextEdit.js';
+import Toolbar from './Toolbar.js';
 
 class Editor extends React.Component {
   constructor(props) {
@@ -12,14 +13,11 @@ class Editor extends React.Component {
   render() {
     return (
       <div>
-        {/* <input type="submit">Back to Documents Portal</input>
-        <h1> This is the document name </h1>
-        <h1> Sharable Document ID: 69696969 </h1>
-        <input type="submit">Save Changes</input> */}
         <button onClick={() => {}}>Back to Documents Portal</button>
         <h1> This is the document name </h1>
         <p> Sharable Document ID: 69696969 </p>
         <button onClick={() => {}}>Save Changes</button>
+        <Toolbar />
         <TextEdit />
       </div>
     );
