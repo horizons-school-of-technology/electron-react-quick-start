16c1552ce82df64be211c897e4932349e464c641
Adam Ripley <aripley@wharton.upenn.edu>
2017-07-24 15:12:06 -0700

    i fuxked up

--- reactApp/components/Toolbar.js
+++ reactApp/components/Toolbar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 // import {Editor, EditorState, RichUtils} from 'draft-js';
 
+const INLINE_STYLES = [
+  {label: 'Bold', style: 'BOLD'},
+  {label: 'Italic', style: 'ITALIC'},
+  {label: 'Underline', style: 'UNDERLINE'},
+  {label: 'Monospace', style: 'CODE'},
+];
+
+
 class Toolbar extends React.Component {
   constructor(props) {
     super(props);
@@ -12,6 +20,17 @@ class Toolbar extends React.Component {
   render() {
     return (
       <div style={divStyle()}>
+        <div className="RichEditor-controls">
+          {INLINE_STYLES.map(type =>
+            <button
+              // key={type.label}
+              // active={currentStyle.has(type.style)}
+              // label={type.label}
+              // onToggle={props.onToggle}
+              style={type.style}
+            />
+          )}
+        </div>
         <button style={btn()} onClick={() => {}}>Bold</button>
         <button style={btn()} onClick={() => {}}>Italic</button>
         <button style={btn()} onClick={() => {}}>Strikethrough</button>
@@ -19,6 +38,13 @@ class Toolbar extends React.Component {
     );
   }
 }
+
+// const InlineStyleControls = (props) => {
+//   var currentStyle = props.editorState.getCurrentInlineStyle();
+//   return (
+//
+//   );
+// };
 
 const divStyle = () => ({
   'border': '2px solid black',
