a21fafff109a3b1b09f830150d170a5efd2b5ee9
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-25 16:16:15 -0700

    more changes to DucmentPortal

--- reactApp/components/DocumentPortal.js
+++ reactApp/components/DocumentPortal.js
@@ -3,22 +3,46 @@ import TextField from 'material-ui/TextField';
 import IconButton from 'material-ui/IconButton';
 import {indigo50, indigo100} from 'material-ui/styles/colors';
 import NewDoc from 'material-ui/svg-icons/action/note-add';
-import {Toolbar, ToolbarGroup, ToolbarSeparator } from 'material-ui/Toolbar';
+import {Toolbar, ToolbarGroup, ToolbarSeparator} from 'material-ui/Toolbar';
+import Paper from 'material-ui/Paper';
+
 // TODO: import {Documents} from 'path to models'
 
 const tempStyles = {
   toolbarStyle: {
     'backgroundColor': '#2481b7',
-    'height': '100px'
+    'height': '80px',
+    'width': '800px',
+    'justifyContent': 'center'
   },
   separator: {
-    'backgroundColor': indigo50
+    'backgroundColor': indigo50,
+    'marginLeft': '15px',
+    'marginRight': '15px',
+    'height': '60px'
   },
   textFieldStyle: {
-    'marginBottom': '5px',
+    'fontColor': indigo50,
+    'fontSize': '1.5em',
+    'width': '550px',
+    'marginRight': '32px'
+  },
+  groupStyle: {
     'display': 'flex',
-    'alignItems': 'center',
-    'fontColor': indigo50
+  },
+  topPaper: {
+    'width': '100%',
+    'height': '150px',
+    'justifyContent': 'center',
+    'alignItems': 'center'
+  },
+  newDocStyle: {
+    'width': '48px',
+    'height': '48px',
+  },
+  newDocButtonStyle: {
+    'width': '80px',
+    'height': '80px'
   }
 
 };
@@ -31,31 +55,32 @@ class DocumentPortal extends React.Component {
   render() {
     return (
       <div>
-        <Toolbar style={tempStyles.toolbarStyle}>
-          <ToolbarGroup>
-            <TextField
-              hintText="I like big butts and I cannot lie"
-              floatingLabelText="Create a new document"
-              floatingLabelStyle={{color: indigo50}}
-              floatingLabelFocusStyle={{color: indigo50}}
-              underlineFocusStyle={{borderColor: indigo50}}
-              hintStyle={{color: indigo100}}
-              style={tempStyles.textFieldStyle}
-            /><br />
-          </ToolbarGroup>
-          <ToolbarGroup>
-            <ToolbarSeparator style={tempStyles.separator}/>
-            <IconButton tooltip="Create Document">
-              <NewDoc color={indigo50}/>
-            </IconButton>
-          </ToolbarGroup>
-        </Toolbar>
+          <div>
+            <Paper style={tempStyles.topPaper} zDepth={1} children={
+              <Toolbar style={tempStyles.toolbarStyle}>
+                <ToolbarGroup style={tempStyles.groupStyle}>
+                  <TextField
+                    hintText="I like big butts and I cannot lie"
+                    underlineStyle={{color: indigo50}}
+                    underlineFocusStyle={{color: indigo50}}
+                    hintStyle={{color: indigo100}}
+                    style={tempStyles.textFieldStyle}
+                  />
+                  <ToolbarSeparator style={tempStyles.separator}/>
+                  <IconButton tooltip="Create Document" iconStyle={tempStyles.newDocStyle} style={tempStyles.newDocButtonStyle}>
+                    <NewDoc color={indigo50}/>
+                  </IconButton>
+                </ToolbarGroup>
+              </Toolbar>
+            }/>
+
+          </div>
         <form action="/action_page.php">
-          <input type="text" name="docName" value="New Document Name"></input>
+          <input type="text" name="docName" placeholder="New Document Name" ></input>
           <input type="submit" value="Submit"></input>
         </form>
         <form action="/action_page.php">
-          <input type="text" name="docID" value="Document ID"></input>
+          <input type="text" name="docID" placeholder="Document ID"></input>
           <input type="submit" value="Submit"></input>
         </form>
       </div>
