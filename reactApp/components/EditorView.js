7cf762b8047c28eae14f83affdb73cb18d072fcf
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-25 09:56:49 -0700

    migrate to mancss

--- reactApp/components/EditorView.js
+++ reactApp/components/EditorView.js
@@ -20,50 +20,20 @@ class EditorView extends React.Component {
 
   render() {
     return (
-      <div style={outsideStyle()}>
-        <div style={centered()}>
+      <div className='outsideStyle'>
+        <div className='centeredTxt'>
           <h1> This is the document name </h1>
           <p> Sharable Document ID: 69696969 </p><br></br>
         </div>
-        <div style={centeredBtn()}>
-          <Route path="/documents" component={DocumentPortal} />
-          <Link role="button" to="/documents" onClick={() => this.onBack()}>Back to Documents Portal</Link>
+        <div className='centeredBtn'>
+          <button onClick={() => this.onBack()}>Back to Documents Portal</button><br></br>
           <button onClick={() => this.onSave()}>Save Changes</button><br></br>
         </div>
         <Toolbar />
-        <div style={textStyle()}><TextEdit/></div>
+        <div className='textStyle'><TextEdit/></div>
       </div>
     );
   }
 }
-
-const centeredBtn = () => ({
-  'display': 'flex',
-  'margin': '20px',
-  'alignItems': 'center',
-  'justifyContent': 'center',
-});
-
-const centered = () => ({
-  'display': 'flex',
-  'flex-direction': 'column',
-  'margin': '20px',
-  'alignItems': 'center',
-  'justifyContent': 'center',
-
-});
-
-const textStyle = () => ({
-  'border': '4px solid black',
-  'margin': '20px',
-  'height': '300px',
-  'background': 'white'
-});
-
-const outsideStyle = () => ({
-  // 'background': '#eef6f6',
-  'height': '100%',
-  'background': '#ededed'
-});
 
 export default EditorView;
