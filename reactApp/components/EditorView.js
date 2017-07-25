0b29b16c2b89d70632c650124be5ecd9b0d518e8
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-24 17:11:37 -0700

    merge conflict avoiding

--- reactApp/components/EditorView.js
+++ reactApp/components/EditorView.js
@@ -2,14 +2,27 @@ import React from 'react';
 import TextEdit from './TextEdit.js';
 import Toolbar from './Toolbar.js';
 
+// TODO: import {Documents} from 'path to models'
+
 class EditorView extends React.Component {
+  onSave() {
+    // TODO: if document exists, update. Else, create and save
+    
+  }
+
+  onBack() {
+    // TODO: if document is not saved, notify user that changes could
+    // be lost. Prompt for save in this modal as well. Continue button in
+    // modal too
+  }
+
   render() {
     return (
       <div>
-        <button onClick={() => {}}>Back to Documents Portal</button>
+        <button onClick={() => this.onBack()}>Back to Documents Portal</button>
         <h1> This is the document name </h1>
         <p> Sharable Document ID: 69696969 </p>
-        <button onClick={() => {}}>Save Changes</button>
+        <button onClick={() => this.onSave()}>Save Changes</button>
         <Toolbar />
         <TextEdit />
       </div>
--- reactApp/components/TextEdit.js
+++ reactApp/components/TextEdit.js
@@ -6,19 +6,6 @@ import {connect} from 'react-redux';
 import {onChangeAction} from '../actions/actions.js'; // import relevant actions
 
 let TextEdit = ({editorState, onChange}) => {
-  // this will be done in reducers
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {editorState: EditorState.createEmpty()};
-  //   this.onChange = (editorState) => this.setState({editorState});
-  // }
-
-  // This will be done in reducers as well (be careful of this)
-  // onBoldClick() {
-  //   this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  // }
-
-  // TODO: this.onChange has to be something different w/ redux
   return (
       <div>
         <Editor
